fa08e197ddcf102d162f7cddebeb8efbc7f2dc2f
thuanhoang07 <zz.mr.thuan.zz@gmail.com>
2025-06-18 16:59:01 +0700

    v3

--- definition.js
+++ definition.js
@@ -1,6 +1,10 @@
+// Import library
+Blockly.Python.definitions_['import_kdi_unoarm'] = 'import kdi_unoarm';
+
+// 1. Set dimensions
 Blockly.Blocks['unoarm_set_dimensions'] = {
   init: function() {
-    this.appendDummyInput().appendField("Set dimensions");
+    this.appendDummyInput().appendField("Set UnoArm dimensions:");
     this.appendValueInput("D1").setCheck("Number").appendField("d1");
     this.appendValueInput("D2").setCheck("Number").appendField("d2");
     this.appendValueInput("D3").setCheck("Number").appendField("d3");
@@ -9,18 +13,79 @@ Blockly.Blocks['unoarm_set_dimensions'] = {
     this.setPreviousStatement(true);
     this.setNextStatement(true);
     this.setColour(230);
-    this.setTooltip("Set the five dimension parameters d1 to d5");
-    this.setHelpUrl("");
+    this.setTooltip("Configure the five physical dimensions of UnoArm");
   }
 };
-
-Blockly.Python.definitions_['import_kdi_unoarm'] = 'import kdi_unoarm';
 Blockly.Python['unoarm_set_dimensions'] = function(block) {
   var d1 = Blockly.Python.valueToCode(block, 'D1', Blockly.Python.ORDER_ATOMIC) || '0';
   var d2 = Blockly.Python.valueToCode(block, 'D2', Blockly.Python.ORDER_ATOMIC) || '0';
   var d3 = Blockly.Python.valueToCode(block, 'D3', Blockly.Python.ORDER_ATOMIC) || '0';
   var d4 = Blockly.Python.valueToCode(block, 'D4', Blockly.Python.ORDER_ATOMIC) || '0';
   var d5 = Blockly.Python.valueToCode(block, 'D5', Blockly.Python.ORDER_ATOMIC) || '0';
-  var code = 'kdi_unoarm.set_dimensions(' + d1 + ', ' + d2 + ', ' + d3 + ', ' + d4 + ', ' + d5 + ')\n';
-  return code;
+  return `kdi_unoarm.set_dimensions(${d1}, ${d2}, ${d3}, ${d4}, ${d5})\n`;
+};
+
+// 2. Check valid angles
+Blockly.Blocks['unoarm_check_valid'] = {
+  init: function() {
+    this.appendValueInput("S2").setCheck("Number").appendField("Check valid for S2");
+    this.appendValueInput("S3").setCheck("Number").appendField("S3");
+    this.setOutput(true, "Boolean");
+    this.setColour(210);
+    this.setTooltip("Return true if angles S2 and S3 are within valid range");
+  }
+};
+Blockly.Python['unoarm_check_valid'] = function(block) {
+  var s2 = Blockly.Python.valueToCode(block, 'S2', Blockly.Python.ORDER_ATOMIC) || '0';
+  var s3 = Blockly.Python.valueToCode(block, 'S3', Blockly.Python.ORDER_ATOMIC) || '0';
+  return [`kdi_unoarm.kiem_tra_tinh_hop_le(${s2}, ${s3})`, Blockly.Python.ORDER_FUNCTION_CALL];
+};
+
+// 3. Move to angles S2, S3
+Blockly.Blocks['unoarm_move_angles'] = {
+  init: function() {
+    this.appendValueInput("TO_S2").setCheck("Number").appendField("Move to angle S2");
+    this.appendValueInput("TO_S3").setCheck("Number").appendField("S3");
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
+    this.setColour(160);
+    this.setTooltip("Move servo joints S2 and S3 to specified angles");
+  }
+};
+Blockly.Python['unoarm_move_angles'] = function(block) {
+  var toS2 = Blockly.Python.valueToCode(block, 'TO_S2', Blockly.Python.ORDER_ATOMIC) || '0';
+  var toS3 = Blockly.Python.valueToCode(block, 'TO_S3', Blockly.Python.ORDER_ATOMIC) || '0';
+  return `kdi_unoarm.go_to_S2_S3(${toS2}, ${toS3})\n`;
+};
+
+// 4. Move to position r, h
+Blockly.Blocks['unoarm_move_r_h'] = {
+  init: function() {
+    this.appendValueInput("R").setCheck("Number").appendField("Move to r");
+    this.appendValueInput("H").setCheck("Number").appendField("h");
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
+    this.setColour(160);
+    this.setTooltip("Move arm to radial (r) and height (h) position");
+  }
+};
+Blockly.Python['unoarm_move_r_h'] = function(block) {
+  var r = Blockly.Python.valueToCode(block, 'R', Blockly.Python.ORDER_ATOMIC) || '0';
+  var h = Blockly.Python.valueToCode(block, 'H', Blockly.Python.ORDER_ATOMIC) || '0';
+  return `kdi_unoarm.go_to_r_h(${r}, ${h})\n`;
+};
+
+// 5. Rotate base
+Blockly.Blocks['unoarm_rotate_base'] = {
+  init: function() {
+    this.appendValueInput("ANGLE").setCheck("Number").appendField("Rotate base to");
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
+    this.setColour(160);
+    this.setTooltip("Rotate base joint S1 to specified angle");
+  }
+};
+Blockly.Python['unoarm_rotate_base'] = function(block) {
+  var angle = Blockly.Python.valueToCode(block, 'ANGLE', Blockly.Python.ORDER_ATOMIC) || '0';
+  return `kdi_unoarm.quay_ve_goc(${angle})\n`;
 };
--- kdi_unoarm.py
+++ kdi_unoarm.py
@@ -1,5 +1,8 @@
 # kdi_unoarm.py
 # Python library for UnoArm control
+import math
+from remote_control import *
+rc_mode = RemoteControlMode(4)
 
 # Default dimensions
 _d1 = 10.25
@@ -10,8 +13,60 @@ _d5 = 7.5
 
 def set_dimensions(d1, d2, d3, d4, d5):
     global _d1, _d2, _d3, _d4, _d5
-    _d1 = d1
-    _d2 = d2
-    _d3 = d3
-    _d4 = d4
-    _d5 = d5
+    _d1, _d2, _d3, _d4, _d5 = d1, d2, d3, d4, d5
+
+
+def kiem_tra_tinh_hop_le(check_S2, check_S3):
+    hop_le = True
+    if check_S3 > 170 or check_S2 > 180:
+        hop_le = False
+    elif (15/16)*check_S2 + check_S3 < 129.375:
+        hop_le = False
+    elif check_S3 < check_S2/2:
+        hop_le = False
+    elif check_S3 > (check_S2*5)/8 + 113.75:
+        hop_le = False
+    return hop_le
+
+
+def go_to_S2_S3(to_S2, to_S3):
+    print(f"Moving to angles S2={to_S2}, S3={to_S3}")
+    if kiem_tra_tinh_hop_le(to_S2, to_S3):
+        init_S2 = servo.position(1)
+        init_S3 = servo.position(2)
+        current_S2, current_S3 = init_S2, init_S3
+        q = math.hypot(init_S2-to_S2, init_S3-to_S3)
+        if q:
+            buoc_S2 = 2*((to_S2-init_S2)/q)
+            buoc_S3 = 2*((to_S3-init_S3)/q)
+            while (init_S2 < to_S2 and current_S2 <= to_S2) or (init_S2 >= to_S2 and current_S2 >= to_S2):
+                current_S2 += buoc_S2
+                current_S3 += buoc_S3
+                servo.position(1, round(current_S2))
+                time.sleep(pause)
+                servo.position(2, round(current_S3))
+                time.sleep(pause)
+            print(f"Updated S2={servo.position(1)}, S3={servo.position(2)}")
+    else:
+        print("Invalid angle combination")
+
+
+def go_to_r_h(r, h):
+    global _d1, _d2, _d3, _d4, _d5
+    # radial-offset and height mapping to S2/S3
+    y1 = r - _d4
+    z1 = h + _d5
+    d6 = math.hypot(y1, z1-_d1)
+    phi2 = math.degrees(math.acos((_d2**2 + (_d3**2 - d6**2))/(2*_d2*_d3)))
+    phi3 = math.degrees(math.acos((_d2**2 + (d6**2 - _d3**2))/(2*_d2*d6)))
+    phi4 = 180 - math.degrees(math.asin(y1/d6)) if z1>_d1 else math.degrees(math.asin(y1/d6))
+    phi1 = phi3 + phi4
+    to_S2 = 2*phi1 - 190
+    to_S3 = phi2 + to_S2/2 - 25
+    go_to_S2_S3(to_S2, to_S3)
+    print(f"Position r={r}, h={h}")
+
+
+def quay_ve_goc(goc_S1):
+    servo.position(0, int(goc_S1))
+    print(f"Base rotated to {goc_S1}")
