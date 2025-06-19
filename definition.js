599f213b438e016d421aa9a3adbf6d89d1b801c6
thuanhoang07 <zz.mr.thuan.zz@gmail.com>
2025-06-19 09:26:11 +0700

    v2.4.1

--- definition.js
+++ definition.js
@@ -1,62 +1,62 @@
-// Định nghĩa block: Set dimensions
-Blockly.Blocks['unoarm_set_dimensions'] = {
+// Import UnoArm library in Python
+Blockly.Python.definitions_['import_kdi_unoarm'] = 'import kdi_unoarm';
+
+// Block: Set UnoArm dimensions\Blockly.Blocks['unoarm_set_dimensions'] = {
   init: function() {
     this.appendDummyInput()
-        .appendField("Set dimensions");
-    this.appendValueInput("D1")
-        .setCheck("Number")
-        .appendField("d1");
-    this.appendValueInput("D2")
-        .setCheck("Number")
-        .appendField("d2");
-    this.appendValueInput("D3")
-        .setCheck("Number")
-        .appendField("d3");
-    this.appendValueInput("D4")
-        .setCheck("Number")
-        .appendField("d4");
-    this.appendValueInput("D5")
-        .setCheck("Number")
-        .appendField("d5");
+        .appendField('Set dimensions');
+    this.appendValueInput('D1')
+        .setCheck('Number')
+        .appendField('d1');
+    this.appendValueInput('D2')
+        .setCheck('Number')
+        .appendField('d2');
+    this.appendValueInput('D3')
+        .setCheck('Number')
+        .appendField('d3');
+    this.appendValueInput('D4')
+        .setCheck('Number')
+        .appendField('d4');
+    this.appendValueInput('D5')
+        .setCheck('Number')
+        .appendField('d5');
     this.setPreviousStatement(true);
     this.setNextStatement(true);
     this.setColour(230);
-    this.setTooltip("Set the five dimension parameters d1 to d5");
-    this.setHelpUrl("");
+    this.setTooltip('Set the five dimension parameters d1 to d5');
+    this.setHelpUrl('');
   }
 };
 
-// Block: Check valid angles S2, S3 (value block)
-Blockly.Blocks['unoarm_check_valid'] = {
-  init: function() {
-    this.appendValueInput("S2")
-        .setCheck("Number")
-        .appendField("Check valid S2");
-    this.appendValueInput("S3")
-        .setCheck("Number")
-        .appendField("S3");
-    // Đây chính là connector hình ‘viên gạch’ để cắm vào khung value
-    this.setOutput(true, "Boolean");
-    this.setColour(210);
-    this.setTooltip("Returns true if the (S2, S3) angles are within valid range");
-    this.setHelpUrl("");
-  }
-};
-
-// Python generator chung
-Blockly.Python.definitions_['import_kdi_unoarm'] = 'import kdi_unoarm';
-
-// Generator cho block Set dimensions
+// Generator: Set dimensions
 Blockly.Python['unoarm_set_dimensions'] = function(block) {
   var d1 = Blockly.Python.valueToCode(block, 'D1', Blockly.Python.ORDER_ATOMIC) || '0';
   var d2 = Blockly.Python.valueToCode(block, 'D2', Blockly.Python.ORDER_ATOMIC) || '0';
   var d3 = Blockly.Python.valueToCode(block, 'D3', Blockly.Python.ORDER_ATOMIC) || '0';
   var d4 = Blockly.Python.valueToCode(block, 'D4', Blockly.Python.ORDER_ATOMIC) || '0';
   var d5 = Blockly.Python.valueToCode(block, 'D5', Blockly.Python.ORDER_ATOMIC) || '0';
-  return 'kdi_unoarm.set_dimensions(' + d1 + ', ' + d2 + ', ' + d3 + ', ' + d4 + ', ' + d5 + ')\n';
+  var code = 'kdi_unoarm.set_dimensions(' + d1 + ', ' + d2 + ', ' + d3 + ', ' + d4 + ', ' + d5 + ')\n';
+  return code;
 };
-// Python generator cho block kiểm tra
-Blockly.Python.definitions_['import_kdi_unoarm'] = 'import kdi_unoarm';
+
+// Block: Check valid angles S2, S3 (value block)
+Blockly.Blocks['unoarm_check_valid'] = {
+  init: function() {
+    this.appendValueInput('S2')
+        .setCheck('Number')
+        .appendField('Check valid S2');
+    this.appendValueInput('S3')
+        .setCheck('Number')
+        .appendField('S3');
+    this.setInputsInline(true);
+    this.setOutput(true, 'Boolean');
+    this.setColour(210);
+    this.setTooltip('Returns true if the (S2, S3) angles are within valid range');
+    this.setHelpUrl('');
+  }
+};
+
+// Generator: Check valid angles
 Blockly.Python['unoarm_check_valid'] = function(block) {
   var s2 = Blockly.Python.valueToCode(block, 'S2', Blockly.Python.ORDER_ATOMIC) || '0';
   var s3 = Blockly.Python.valueToCode(block, 'S3', Blockly.Python.ORDER_ATOMIC) || '0';
--- kdi_unoarm.py
+++ kdi_unoarm.py
@@ -2,41 +2,21 @@
 # Python library for UnoArm control
 
 # Default dimensions
-_d1 = 10.25
-_d2 = 15
-_d3 = 16
-_d4 = 4.64
-_d5 = 7.5
+d1, d2, d3, d4, d5 = 10.25, 15, 16, 4.64, 7.5
 
-#khai bao vi tri ban dau
-y_now = 28 #tam xa
-z_now = 9 #tam cao
-phi_now = 90 #goc
-topic_hut = None
-step = 0.1
-#khai bao cac hang so khac
-pause = 0.01
-pause2 = 0.01
-cong_suat_hut = 100
+# Function: set physical dimensions
+def set_dimensions(_d1, _d2, _d3, _d4, _d5):
+    global d1, d2, d3, d4, d5
+    d1, d2, d3, d4, d5 = _d1, _d2, _d3, _d4, _d5
 
-def set_dimensions(d1, d2, d3, d4, d5):
-    global _d1, _d2, _d3, _d4, _d5
-    _d1 = d1
-    _d2 = d2
-    _d3 = d3
-    _d4 = d4
-    _d5 = d5
-    
+# Function: check if angles S2, S3 are valid
 def kiem_tra_tinh_hop_le(check_S2, check_S3):
-  hop_le = True
-  if check_S3 > 170:
-    hop_le = False
-  elif check_S2 > 180:
-    hop_le = False
-  elif (15 / 16) * check_S2 + check_S3 < 129.375:
-    hop_le = False
-  elif check_S3 < check_S2 / 2:
-    hop_le = False
-  elif check_S3 > (check_S2 * 5) / 8 + 113.75:
-    hop_le = False
-  return hop_le
+    if check_S3 > 170 or check_S2 > 180:
+        return False
+    if (15/16)*check_S2 + check_S3 < 129.375:
+        return False
+    if check_S3 < check_S2/2:
+        return False
+    if check_S3 > (check_S2*5)/8 + 113.75:
+        return False
+    return True
